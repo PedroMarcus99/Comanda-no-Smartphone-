4ce26f9724c49ba32af298da38018bb210c1ebb3
PedroMarcus99 <pedromarcussilva38@gmail.com>
2016-11-14 15:41:05 -0200

    MainActivity

--- ComandaSmatphone/ComandaSmatphone/Resources/Resource.Designer.cs
+++ ComandaSmatphone/ComandaSmatphone/Resources/Resource.Designer.cs
@@ -57,6 +57,28 @@ namespace ComandaSmatphone
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050002
+			public const int btnCliente = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int btnEstabelecimento = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayotMainActivity = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
